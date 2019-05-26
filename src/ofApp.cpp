fad045ccfbc937de17e36fcad9627d2d35eefca9
MrAceitun0 <aceitunodev@gmail.com>
2019-05-26 20:13:45 +0200

    Add files via upload

---
+++ src/ofApp.cpp
@@ -0,0 +1,129 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofEnableDepthTest();
+	ofSetVerticalSync(true);
+
+	cam.setGlobalPosition(glm::vec3(0, 0, 0));
+	cam.tilt(70);
+
+	player = new Player();
+	floor = new Floor();
+
+	player->gravity = -10.0;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	/*
+	if (zVelocity <= 0)
+		zVelocity = 0;
+	if(cam.getGlobalPosition().z > 225)
+		zVelocity = zVelocity + gravity;
+
+	cam.setPosition(cam.getGlobalPosition().x, cam.getGlobalPosition().y + yVelocity, cam.getGlobalPosition().z + zVelocity);
+	*/
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ofDrawAxis(100);
+	cam.begin();
+
+	player->render();
+	floor->render();
+
+	cam.end(); 
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+	if (key == 's')
+		player->zVelocity = 25.0;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+Player::Player()
+{
+	position = glm::vec3(0, 0, 15);
+}
+
+void Player::render()
+{
+	if (zVelocity < 0)
+		zVelocity = 0;
+	else if (position.z > 15)
+		zVelocity += gravity;
+
+	ofSetColor(0, 255, 0);
+	ofFill();
+	position = glm::vec3(0, position.y + yVelocity, position.z + zVelocity);
+	ofDrawSphere(position, 15);
+}
+
+void Player::update()
+{
+}
+
+Floor::Floor()
+{
+	
+}
+
+void Floor::render()
+{
+	ofSetColor(255, 0, 0);
+	ofFill();
+	ofDrawPlane(position, 100, 100);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+
+class Player;
+class Floor;
+
+class ofApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofEasyCam cam;
+
+		Player* player;
+		Floor* floor;
+};
+
+class Player
+{
+	public:
+		Player();
+		ofNode node;
+		glm::vec3 position = glm::vec3(0,0,0);
+		float yVelocity = 1;
+		float zVelocity = 0;
+		float gravity;
+		float hitbox = 30;
+		void render();
+		void update();
+};
+
+class Floor
+{
+	public:
+		Floor();
+		glm::vec3 position;
+		float size;
+		void render();
+};
